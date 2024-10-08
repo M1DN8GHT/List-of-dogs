b8e0ab5e277feee7a829a142e5feb129255994ec
M1DN8GHT <mjydunk@gmail.com>
2024-10-08 11:12:12 -0500

    Change

--- main.py
+++ main.py
@@ -2,12 +2,12 @@
 Module Docstring: main.py
 ======================
 
-This is the main module of the read/write to the file project.
+This is the main module of the read/write to the text file project.
 """
 
 __author__ = "Manase Yaya"
 __version__ = "0.1"
-__date__ = "October 7, 2024"
+__date__ = "October 8, 2024"
 __license__ = "None"
 
 def read_file(file_name) -> None:
@@ -18,23 +18,34 @@ def read_file(file_name) -> None:
         for line in f:
             print(line, end="")
 
-def write_file(file_name) -> None:
+def new_file(file_name) -> None:
     """
-    Writes a text file and prints it to the console.
+    Creates a new text file and writes data to it, Or overwrites an existing file.
     """
     with open(file_name, "w") as f:
-        f.write("Pit Bull")
+        f.write("Maine Coon")
     
+def append_file(file_name, data) -> None:
+    """
+    Appends data to an existing text file.
+    """
+    with open(file_name, "r") as f:
+        lines = f.readlines()
+
+    with open(file_name, "a") as f:
+        if lines and lines[-1].strip()
+        if lines and last_line !="":
+            f.write("\n")
+        f.write(data)  
 def main():
     """
     Main entry point of the application.
     """
-    write_file("dogs.txt")
-
-    pass
+    append_file("cats.txt", "Burmese")
 
 if __name__ == "__main__":
     """
     Starts the program.
     """
     main()
+
